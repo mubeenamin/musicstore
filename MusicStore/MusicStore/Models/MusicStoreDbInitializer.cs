37491352f62e840a0e1190dc649722a507627e18
mubeenamin <mubeenamin6@gmail.com>
2015-03-31 21:23:17 +0500

    make cs file for music store database initializer

---
+++ MusicStore/MusicStore/Models/MusicStoreDbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore.Models
+{
+    public class MusicStoreDbInitializer : System.Data.Entity.DropCreateDatabaseAlways<MusicStoreDB>
+    {
+        protected override void Seed(MusicStoreDB context)
+        {
+            context.Artists.Add(new Artist { Name = "Ali Da Malang" });
+            context.Genres.Add(new Genre { Name = "Jazz" });
+            context.Albums.Add(new Album
+            {
+                Artist = new Artist { Name = "Rush" },
+                Genre = new Genre { Name = "Rock" },
+                Price = 100m,
+                Title = "Mubeen"
+            }
+                );
+            base.Seed(context);
+        }
+    }
+}
